89d68ad35ce1eb0b624d1873ba754cf75a5c39c4
Viki166 <55790121+Viki166@users.noreply.github.com>
2020-11-27 16:53:46 +0300

    Add files via upload

---
+++ petHouse.py
@@ -0,0 +1,5 @@
+class Cat:
+    def __init__(self, name, gender='Мальчик', age=2):
+        self.name = name
+        self.gender = gender
+        self.age = age
---
+++ testPetHouse.py
@@ -0,0 +1,13 @@
+from petHouse import Cat
+
+
+baron = Cat('Барон')
+print(baron.name)
+print(baron.gender)
+print(baron.age)
+
+sem = Cat('Сэм')
+print(sem.name)
+print(sem.gender)
+print(sem.age)
+
