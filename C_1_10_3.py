a44c96ce2e24ec6b78b74e6e2852f7ea28cf3619
Viki166 <55790121+Viki166@users.noreply.github.com>
2020-12-07 20:25:08 +0300

    Update C_1_10_3.py

--- C_1_10_3.py
+++ C_1_10_3.py
@@ -4,22 +4,23 @@ class Customer:
         self.name = name
         self.balance = balance
 
-    def data(self):
+    def set_data(self):
         self.name = str(input("Введите фамилию, имя и отчество: "))
         self.balance = float(input("Ваш баланс: "))
         return f"Клиент: {self.name}. Баланс: {self.balance}."
 
-    def clientData(self):
+    def get_data(self):
             return f"Клиент: {self.name}. Баланс: {self.balance}."
+    
 while True:
     value = input("Введите 1, чтобы добавить клиента или 2, чтобы вывести данные о клиенте: ")
     if value == '1':
         client = Customer("", 0)
-        print(client.data())
+        print(client.set_data())
         break
     elif value == '2':
         client = Customer("Иванов Иван Иванович", 500)
-        print(client.clientData())
+        print(client.get_data())
         break
     else:
         print("Значение введено некорректно")
